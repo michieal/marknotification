e231d207ce40259e06f129e6a2f3998b68d2cec1
Michieal <michieal@apophistech.com>
2022-03-06 18:41:05 -0500

    v6.1.2.0
    V6.1.2.0 ~ Added javascript to perform the calls so that the page doesn't have to refresh each time you mark or delete a notification.
    
    Added the Delete All Reactions as a button on the bottom of the Notifications dropdown, next to the See All link.

---
+++ plugins/default/css/marknotif.php
@@ -0,0 +1,49 @@
+/*Apophis Software*/
+
+.ossn-notification-page .notification-image,
+.ossn-notification-page .notification-image img {
+display: inline-block;
+margin-right: 5px;
+}
+.ossn-notification-page .notfi-meta{
+display: inline-block;
+}
+.apop-notif-delete{
+float:left;
+position:relative;
+display:inline;
+}
+.apop-notif-dismiss, .apop-notif-read{
+float:right;
+position:relative;
+display:inline;
+}
+.apop-notif-unread{
+text-align: center;
+text-anchor: middle;
+position:relative;
+}
+.apop-hr{
+color: #777;
+}
+
+.apop-notif-delete-button{
+float:right;
+position:relative;
+display:inline;
+background-color: red;
+color: white;
+border: 1px solid white;
+border-radius: 3px;
+padding: 5px
+}
+
+.apop-bottom-link{
+float:right;
+position:relative;
+display:inline;
+border: 1px solid white;
+border-radius: 3px;
+margin-top: -5px;
+margin-right: 5px;
+}
